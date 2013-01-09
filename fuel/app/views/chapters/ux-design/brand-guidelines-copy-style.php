5ecccdeddb68decb2d71aaeadd39e69a9a6b1c68
Thomas Edwards <thomas.edwards@gmail.com>
2013-01-09 22:51:21 +0000

    Brand Guidelines and Copy Style

--- fuel/app/views/chapters/ux-design/brand-guidelines-copy-style.php
+++ fuel/app/views/chapters/ux-design/brand-guidelines-copy-style.php
@@ -1,1 +1,1 @@
-                    <p>brand-guidelines-copy-style</p>
+                    <p>Take notes and provide a reference to brand guidelines and the style of copy. For the project, agree on how the project name and related entities should be written. For example, is it ‘iPhone’ or ‘I-phone’?</p>
