22559316297f7d77cb84c6df1542bba32dbf868b
Thomas Edwards <thomas.edwards@gmail.com>
2013-01-09 22:54:27 +0000

    Tech spec: client browser services and server

---
+++ fuel/app/views/chapters/technical-specification/client-browser-devices.php
@@ -0,0 +1,1 @@
+				<p>List which browsers will be used in testing, and on what platforms. List features that won’t work across all browsers if applicable.</p>
---
+++ fuel/app/views/chapters/technical-specification/server.php
@@ -0,0 +1,8 @@
+				<p>Things to consider when deciding on the server choice:</p>
+				<ul>
+					<li>Who is responsible for the servers?</li>
+					<li>Where are they located?</li>
+					<li>What operating system should they run?</li>
+					<li>Are they shared, cloud or dedicated?</li>
+					<li>What are the security risks?</li>
+				</ul>
