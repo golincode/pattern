f2dec71f6ecef1b60f117835dc95d3cd3e25ffd0
Thomas Edwards <thomas.edwards@gmail.com>
2013-01-09 22:42:36 +0000

    Added to “what is Pattern?”

--- fuel/app/views/chapters/preface/what-is-pattern.php
+++ fuel/app/views/chapters/preface/what-is-pattern.php
@@ -1,1 +1,2 @@
 	            <p>Pattern is a guide to Architect’s ways of working and best practice.</p>
+	            <p>It’s a set of guides to help you document your projects. Everything in Pattern should be considered and those considerations written down.</p>
