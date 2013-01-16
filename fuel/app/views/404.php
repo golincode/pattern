af9ed8beb3837c096a816553f69f928d2b451f75
Thomas Edwards <thomas@fuse.co.uk>
2013-01-16 16:10:42 +0000

    Added note about where Pattern can be used (LDN, SFO)

--- fuel/app/views/404.php
+++ fuel/app/views/404.php
@@ -2,5 +2,6 @@
             <section class="chapter">
                 <h1>Error 404</h1>
                 <p>Page not found</p>
+                <p>Pattern can only be used from Architect London and San Francisco offices.</p>
             </section>
         </article>
