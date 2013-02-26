b33e6de8f3c0fcc5a3d7bb60ebddea3b65e20e59
Thomas Edwards <thomas.edwards@gmail.com>
2013-02-26 23:15:04 +0000

    Added note to design process

--- fuel/app/views/chapters/ux-design/design-process.php
+++ fuel/app/views/chapters/ux-design/design-process.php
@@ -1,1 +1,3 @@
                     <p>You should describe to the client how the design process works.</p>
+                    <p>List how many rounds of feedback are included and how long that process should last.</p>
+                    
