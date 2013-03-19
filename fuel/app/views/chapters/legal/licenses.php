03a08211082c3080f5c7fff3c988ef968a3af679
Thomas Edwards <thomas.edwards@gmail.com>
2013-03-19 22:54:30 +0000

    Added licenses

---
+++ fuel/app/views/chapters/legal/licenses.php
@@ -0,0 +1,24 @@
+				<p>A license for each library, framework or software included with the application being built must be recorded. Place each license as a text file in a ‘Licenses’ folder in ‘Admin’.</p>
+				<p>The format for each license is as follows:<br />
+					[Software] [Software version] [Licence + version].txt
+				</p>
+				<blockquote class="example example-sample">
+					<p>
+						PHP 5.4 The PHP License 3.01.txt<br />
+						jQuery 1.9.1 MIT License.txt
+					</p>
+				</blockquote>
+				<p>Remember to take note of the software version and the license number, if applicable.</p>
+				<p>If two pieces of software use the same license (for example, MIT) you must include one for each piece of software in full.</p>
+				<p>Here are links to the most popular licenses you’ll need:</p>
+				<ul>
+					<li><a href="http://php.net/license/index.php">PHP Licensing</a></li>
+					<li><a href="https://jquery.org/license/">jQuery License</a></li>
+					<li><a href="https://raw.github.com/joyent/node/v0.10.0/LICENSE">Node</a></li>
+					<li><a href="http://www.ruby-lang.org/en/about/license.txt">Ruby</a></li>
+					<li><a href="http://opensource.org/licenses/mit-license.php">Ruby on Rails</a></li>
+					<li><a href="http://fuelphp.com/docs/license.html">FuelPHP</a></li>
+					<li><a href="http://www.apache.org/licenses/LICENSE-2.0">Twitter Bootstrap</a></li>
+					<li><a href="http://opensource.org/licenses/mit-license.php">Socket.IO</a></li>
+					<li><a href="https://github.com/documentcloud/backbone/blob/master/LICENSE">Backbone.js</a></li>
+				</ul>
