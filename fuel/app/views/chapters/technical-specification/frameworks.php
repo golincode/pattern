55bbb30f4744ea2989772992305f1529a8ab5834
Thomas Edwards <thomas.edwards@gmail.com>
2013-03-19 23:06:29 +0000

    Added note about software licence

--- fuel/app/views/chapters/technical-specification/frameworks.php
+++ fuel/app/views/chapters/technical-specification/frameworks.php
@@ -1,1 +1,2 @@
 				<p>Similar to above, but for the application framework and any libraries used. Typically these are part of the repository and travel with the application.</p>
+				<p>Also include links to the licences of the libraries and frameworks and take a copy of them. See the Legal chapter for more.</p>
--- fuel/app/views/chapters/technical-specification/software.php
+++ fuel/app/views/chapters/technical-specification/software.php
@@ -1,3 +1,4 @@
 				<p>List software required for the application to run, such as what’s on the server, and any software required for development.</p>
 				<p>For example, on the server might be PHP and Apache, and for development Sass.</p>
-				<p>Also list the minimum version numbers.</p>
+				<p>List the minimum version numbers.</p>
+				<p>Also include links to the licences of the software and take a copy of them. See the Legal chapter for more.</p>
