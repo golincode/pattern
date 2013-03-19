f5a70bfdb7a67719c370b69090cc7a4b69669789
Thomas Edwards <tedwards@wearearchitect.com>
2013-03-19 11:42:34 +0000

    Added note about IPv6

--- fuel/app/views/chapters/technical-specification/domains-dns.php
+++ fuel/app/views/chapters/technical-specification/domains-dns.php
@@ -1,5 +1,6 @@
 				<p>List domains used for each environment, who’s responsible for buying them if applicable, and who manages the DNS.</p>
 				<p>Explain the relationship between the nameservers and records, who is responsible for them and suggest what they should be.</p>
+				<p>Remember, all records should now include IPv6 addresses. If the server doesn’t support IPv6, make a note for future reference.</p>
 <!--
 				<blockquote class="example example-sample">
 					<p>A name</p>
