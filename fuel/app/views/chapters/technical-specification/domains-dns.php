a73fe24d75f36a12d2365f8cec8cd05d951616c7
Thomas Edwards <tedwards@wearearchitect.com>
2013-05-13 16:31:14 +0100

    Added note about domains

--- fuel/app/views/chapters/technical-specification/domains-dns.php
+++ fuel/app/views/chapters/technical-specification/domains-dns.php
@@ -1,3 +1,9 @@
+				<p>
+					<strong>NB</strong> Don’t buy domain names for a client 
+					without their explicit permission. Some clients have to 
+					own the domain names in order to use them, and transferring 
+					them back is sometimes costly and adds a delay.
+				</p>
 				<p>List domains used for each environment, who’s responsible for buying them if applicable, and who manages the DNS.</p>
 				<p>Explain the relationship between the nameservers and records, who is responsible for them and suggest what they should be.</p>
 				<p>Remember, all records should now include IPv6 addresses. If the server doesn’t support IPv6, make a note for future reference.</p>
